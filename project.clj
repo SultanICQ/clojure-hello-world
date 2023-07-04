5ded2c5fe0faaef9681813db5dd7e80abf883182
David Garcia Latorre <sultanicq@gmail.com>
2023-07-04 22:32:30 +0200

    Signing test

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject hello-world "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Hello world clojure test"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
@@ -7,4 +7,4 @@
   :main ^:skip-aot hello-world.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})˘
